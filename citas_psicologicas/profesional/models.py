75f860eeeadccd6ad498015bf540dc4ea4eb077f
RedzukiVei <jquispel@ulasalle.edu.pe>
2026-05-04 03:23:28 -0500

    Empezando con el avance del model para profesionales

--- citas_psicologicas/profesional/models.py
+++ citas_psicologicas/profesional/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from sistema.models import Especialidad
 
-# Create your models here.
+class Profesional(models.Model):
+    """Psicólogo o profesional de salud mental registrado en el sistema"""
+ 
+    GENERO_CHOICES = [
+        ('M', 'Masculino'),
+        ('F', 'Femenino'),
+        ('O', 'Otro'),
+    ]
+ 
+    MODALIDAD_CHOICES = [
+        ('PRESENCIAL', 'Presencial'),
+        ('VIRTUAL',    'Virtual'),
+        ('AMBAS',      'Presencial y Virtual'),
+    ]
