f3812668c4e52e92f11d86d6ac1b5ab8de494e2a
RedzukiVei <jquispel@ulasalle.edu.pe>
2026-05-04 03:27:10 -0500

    Avanzando con los modelos para los datos personales y contactos.

--- citas_psicologicas/profesional/models.py
+++ citas_psicologicas/profesional/models.py
@@ -15,3 +15,14 @@ class Profesional(models.Model):
         ('VIRTUAL',    'Virtual'),
         ('AMBAS',      'Presencial y Virtual'),
     ]
+
+    nombres = models.CharField(max_length=100, verbose_name="Nombres")
+    apellidos = models.CharField(max_length=100, verbose_name="Apellidos")
+    dni = models.CharField(max_length=15, unique=True, verbose_name="DNI / Documento")
+    fecha_nacimiento = models.DateField(verbose_name="Fecha de Nacimiento")
+    genero = models.CharField(max_length=1, choices=GENERO_CHOICES, verbose_name="Género")
+
+    telefono = models.CharField(max_length=20, verbose_name="Teléfono")
+    email = models.EmailField(unique=True, verbose_name="Correo Electrónico")
+
+    
