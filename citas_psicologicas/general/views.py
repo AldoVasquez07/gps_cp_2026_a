07b73b810ab142c3b4d396a23fa23f3114a874ee
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2026-05-05 19:03:05 -0500

    Integrando vista para la selección del tipo de usuario

--- citas_psicologicas/general/views.py
+++ citas_psicologicas/general/views.py
@@ -6,3 +6,12 @@ from django.shortcuts import render
 def main_content_page(request):
     """Renderiza la página principal del sistema."""
     return render(request, 'general/main_page_citas.html')
+
+
+# -------------------------------------------------------------
+# Selección del tipo de usuario
+# -------------------------------------------------------------
+def seleccion_tipo_usuario(request):
+    """Muestra la página para elegir el tipo de usuario (cliente, profesional)."""
+    return render(request, 'general/seleccion_tipo_usuario.html')
+
