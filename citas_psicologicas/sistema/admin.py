82affb306f2c11aee664057fcde603d6b62bef99
Rodrigo <103969147+RodrigoRuelas@users.noreply.github.com>
2026-05-10 19:45:48 -0500

    Actulización admin.py: Importando las funciones de models.py

--- citas_psicologicas/sistema/admin.py
+++ citas_psicologicas/sistema/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
-from .models import ClinicaConfig, Especialidad
-
-@admin.register(ClinicaConfig)
-class ClinicaConfigAdmin(admin.ModelAdmin):
-    # Esto evita que creen múltiples configuraciones (solo debe haber una)
-    def has_add_permission(self, request):
-        if self.model.objects.count() >= 1:
-            return False
-        return True
-    
-    list_display = ('nombre_institucion', 'telefono_contacto', 'email_contacto')
-
-@admin.register(Especialidad)
-class EspecialidadAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'descripcion')
-    search_fields = ('nombre',)
+from .models import (
+    Pais, Ciudad, Rol, Usuario,
+    LogProcesos, RegistroAcceso,
+    AspectosNegocio, Disponibilidad
+)
