30af65d43d73b1d31199f21aeeaa28221a21f825
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2026-05-19 11:18:52 -0500

    Ajustando archivos de configuración para despliegue: asgi.py y wsgi.py

--- citas_psicologicas/citas_psicologicas/asgi.py
+++ citas_psicologicas/citas_psicologicas/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'citas_profesionales.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'citas_psicologicas.settings')
 
 application = get_asgi_application()
--- citas_psicologicas/citas_psicologicas/wsgi.py
+++ citas_psicologicas/citas_psicologicas/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'citas_profesionales.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'citas_psicologicas.settings')
 
 application = get_wsgi_application()
 
